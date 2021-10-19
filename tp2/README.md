8a4f11e12b720319779f536b9f060689499f0cfe
amveiga <amveiga@fi.uba.ar>
2021-10-19 10:04:48 -0300

    Modifica main.py a un nombre más descriptivo. Elimina archivos innecesarios. Un poco más de descripción en el README del tp2

--- tp2/README.md
+++ tp2/README.md
@@ -3,3 +3,20 @@
 Para imprimir una matriz en el tp, incluimos el modulo `pandas` que se puede instalar utilizando el script `dependencies.sh`:
 
 ``` sh dependencies.sh ```
+
+### Formato de los archivos:
+
+El programa debe recibir por parámetro el path del archivo donde se encuentran los costos entre cada depósito. El archivo debe ser de tipo texto y presentar por renglón, separados por coma un par de depósitos con su distancia.
+
+Ejemplo: "depositos.txt"
+
+```
+A,B,54
+A,D,-3
+B,C,8
+...
+```
+
+### Ejemplo de invocación:
+
+python3 minimizando_costos.py -f *Path al archivo de ciudades* --file *nombre_del_archivo.txt* 
--- tp2/caminos_minimos.py
+++ tp2/caminos_minimos.py
@@ -1,14 +1,12 @@
 from grafo import *
 import heapq
 
-
 def obtener_aristas(grafo: Grafo):
     aristas = []
     for v in grafo.obtener_vertices():
         for w in grafo.obtener_adyacentes(v):
             aristas.append((v, w, grafo.obtener_peso(v, w)))
     return aristas
-
 
 def camino_minimo_bf(grafo: Grafo, origen):
     distancia = {}
@@ -27,22 +25,17 @@ def camino_minimo_bf(grafo: Grafo, origen):
         if distancia[v] + peso < distancia[w]:
             return None, None  # Si vuelvo a iterar y encuentro una mejoria hay un ciclo negativo
     return padre, distancia
-
     # complejidad O(V*E)
-
 
 def camino_minimo_dijkstra(grafo: Grafo, origen):
     dist = {}
     padre = {}
-
     for v in grafo.obtener_vertices():
         dist[v] = float("inf")  # inicializo todos los vertices en el infinito
-
     dist[origen] = 0
     padre[origen] = None
     q = []
     heapq.heappush(q, (0, origen))
-
     while len(q) != 0:
         v = heapq.heappop(q)
         for w in grafo.obtener_adyacentes(v[1]):
@@ -52,7 +45,6 @@ def camino_minimo_dijkstra(grafo: Grafo, origen):
                 heapq.heappush(q, (dist[w], w))
                 # o sino: q.actualizar(w, dist[w])
     return padre, dist
-
 
 def camino_minimo_johnson(grafo: Grafo):
     v = grafo.vertice_aleatorio()
@@ -74,14 +66,11 @@ def camino_minimo_johnson(grafo: Grafo):
                 distancias_totales[v][w] = distancias_totales[v][w] + distancia[w] - distancia[v]
         return distancias_totales
 
-
 def obtener_vertice_ideal(diccionario):
     distancias = {}
     for dep,dic in diccionario.items():
         distancias[dep] = 0
         for k,v in dic.items():
             distancias[dep] = distancias[dep] + v
-   
     vertice_ideal = min(distancias.items(), key=lambda x: x[1])
     return vertice_ideal[0]
-
