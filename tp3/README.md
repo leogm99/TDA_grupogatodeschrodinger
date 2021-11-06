78cc6c9de26354a03ffaadf4b84331c091ce1802
leogm99 <lgiampieri62@gmail.com>
2021-11-06 13:08:38 -0300

    Agrego parametros y readme

---
+++ tp3/README.md
@@ -0,0 +1,22 @@
+# Una campaña publicitaria masiva pero mínima
+
+Resolución del problema enunciado mediante reducción a un problema de flujo, utilizando Ford-Fulkerson.
+
+
+Instrucciones de ejecución:
+
+```
+> python3 main.py -h
+usage: main.py [-h] -f FILE
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -f FILE, --file FILE  path/to/<nombre_archivo.txt>
+
+> python3 main.py -f test.txt
+Se debe poner un cartel publicitario entre F y E
+Se debe poner un cartel publicitario entre F y B
+Se debe poner un cartel publicitario entre C y E
+En total, se deben poner 3 carteles publicitarios
+Cantidad máxima de pasajeros cubierta con la selección mínima de vuelos entre aeropuertos en donde poner los carteles: 23
+```
--- tp3/main.py
+++ tp3/main.py
@@ -1,5 +1,6 @@
 from grafo import Grafo
 import max_flow
+import argparse
 
 
 # se redujo el problema a max_flow (polinomialmente)
@@ -8,23 +9,25 @@ import max_flow
 # el grafo residual otorga las aristas del corte minimo
 # (son las que van desde un vertice alcanzable por la fuente a un vertice no alcanzable)
 # estas se pueden encontrar con dfs/bfs
-# TODO: args
 def main():
-    # para este grafo se espera un max_flow de 23
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--file', dest='file', help='path/to/<nombre_archivo.txt>', required=True)
+    args = parser.parse_args()
+    # para el grafo test.txt se espera un max_flow de 23
     # ademas debo cortar las aristas (F, E), (F, B) y (C, E)
     # de tal manera de generar un corte minimo (el corte de menor costo posible)
     # en estas aristas debería poner las publicidades
     # me aseguro que TODOS los que van de s a t vean los carteles ya que
     # estas aristas cortan al grafo en dos componentes conexas, una contiene a s y otra contiene a t
 
-    g = load_graph('test.txt')
-    print(g)
+    g = load_graph(args.file)
 
     # despues de esto, g es modificado (por ser puntero)!
     # g se transforma en el grafo residual
     flow = max_flow.max_flow_ford_fulkerson(g, 'A', 'B')
-    print(f"El costo total de los carteles será {flow}")
     print_solution(max_flow.modified_dfs(g, 'A'))
+    print(f"Cantidad máxima de pasajeros cubierta con la selección mínima de vuelos entre aeropuertos en donde poner "
+          f"los carteles: {flow}")
 
 
 def load_graph(filename):
@@ -46,7 +49,8 @@ def load_graph(filename):
 
 
 def print_solution(non_reachable):
-    [print(f"Poner un cartel publicitario entre {u} y {v}") for u, v in non_reachable]
+    _ = [print(f"Se debe poner un cartel publicitario entre {u} y {v}") for u, v in non_reachable]
+    print(f"En total, se deben poner {len(_)} carteles publicitarios")
 
 
 if __name__ == '__main__':
